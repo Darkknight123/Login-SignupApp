4c325637107bc999b510a6524b1d450163560a62
kerryphilip <philipkerry57@gmail.com>
2021-06-10 20:03:24 +0300

    custom adapters

---
+++ Adapter/app/src/main/java/com/example/adapter/AndroidVersion.java
@@ -0,0 +1,23 @@
+package com.example.adapter;
+
+public class AndroidVersion {
+    private double versionNumber;
+
+    public double getVersionNumber() {
+        return versionNumber;
+    }
+
+    public void setVersionNumber(double versionNumber) {
+        this.versionNumber = versionNumber;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+
+    private String  versionName;
+}
---
+++ Adapter/app/src/main/java/com/example/adapter/customAdapter.java
@@ -0,0 +1,55 @@
+package com.example.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class customAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<AndroidVersion>versionArrayList;
+    private Object view;
+    private ViewGroup viewGroup;
+
+    public customAdapter (Context context, ArrayList<AndroidVersion> versionArrayList){
+        this.context=context;
+        this.versionArrayList=versionArrayList;
+
+
+    }
+
+    @Override
+    public int getCount() {
+        if (versionArrayList!=null&&versionArrayList.size()>0){
+            return versionArrayList.size();
+        }else {
+            return 0;
+        }
+
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return versionArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (view==null){
+            view = LayoutInflater.from(context).inflate(R.layout.versionlistitem, viewGroup, false);
+        }
+        TextView txtVersionNumber= view.findViewById(R.id.tv_versionNumber);
+        TextView txtVersionNAme=view.findViewById(R.id.tv_versionName);
+        return  view;
+    }
+}
