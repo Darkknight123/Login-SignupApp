c6fc2f8137e49bdf499408ae097a2d7d3e0c3514
kerryphilip <philipkerry57@gmail.com>
2021-06-24 23:52:59 +0300

    worked on the errors on fragments

--- Fragments/app/src/main/java/com/example/fragments/DemoFragment.java
+++ Fragments/app/src/main/java/com/example/fragments/DemoFragment.java
@@ -3,12 +3,13 @@ package com.example.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
 
 public class DemoFragment extends Fragment {
 
