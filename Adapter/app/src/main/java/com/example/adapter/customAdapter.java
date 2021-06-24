83f1ceec17f35ea3904e27f51c1212beed864984
kerryphilip <philipkerry57@gmail.com>
2021-06-24 23:29:27 +0300

    started on fragments

--- Adapter/app/src/main/java/com/example/adapter/customAdapter.java
+++ Adapter/app/src/main/java/com/example/adapter/customAdapter.java
@@ -4,13 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class customAdapter extends BaseAdapter {
+    private Object androidVersions = new Object();
     private Context context;
     private ArrayList<AndroidVersion>versionArrayList;
     private Object view;
@@ -21,6 +21,24 @@ public class customAdapter extends BaseAdapter {
         this.versionArrayList=versionArrayList;
 
 
+        androidVersions = null;
+    }
+
+    public <androidVersions> customAdapter(androidVersions) {
+        return customAdapter( );
+    }
+
+    public customAdapter(MainActivity context, Object androidVersions) {
+        this.context = context;
+        this.androidVersions = androidVersions;
+    }
+
+    public customAdapter(MainActivity mainActivity, void androidVersions) {
+    }
+
+    public <androidVersions> Object customAdapter(androidVersions Object, androidVersions) {
+        this.androidVersions = androidVersions;
+        return this.androidVersions;
     }
 
     @Override
@@ -48,12 +66,17 @@ public class customAdapter extends BaseAdapter {
         if (view==null){
             view = LayoutInflater.from(context).inflate(R.layout.versionlistitem, viewGroup, false);
         }
-        TextView txtVersionNumber= view.findViewById(R.id.tv_versionNumber);
-        TextView txtVersionName=view.findViewById(R.id.tv_versionName);
+       TextView txtVersionNumber= findViewById();
+        TextView txtVersionName=findViewById();
 
-        int i;
+        int i = 0;
         txtVersionName.setText(versionArrayList.get(i).getVersionName());
         txtVersionNumber.setText("version"+versionArrayList.get(i).getVersionNumber());
         return (View) view;
+    }
+
+    private TextView findViewById() {
+
+        return null;
     }
 }
---
+++ Fragments/app/src/main/java/com/example/fragments/DemoFragment.java
@@ -0,0 +1,27 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DemoFragment extends Fragment {
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+       View view =inflater.from(getContext()).inflate(R.layout.fragment_layout,container,false);
+
+        ArrayAdapter <String> arrayAdapter= new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.android_version));
+
+        ListView listView = view.findViewById(R.id.list_view);
+        listView.setAdapter(arrayAdapter);
+        return view;
+    }
+}
