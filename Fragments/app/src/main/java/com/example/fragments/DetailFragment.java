01841a54c48526a489ac8ad6b11aaf1a75577985
kerryphilip <philipkerry57@gmail.com>
2021-07-01 23:22:10 +0300

    created another fragment

---
+++ Fragments/app/src/main/java/com/example/fragments/DetailFragment.java
@@ -0,0 +1,27 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.jetbrains.annotations.NotNull;
+
+public class DetailFragment extends Fragment {
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+
+        View view= inflater.from(getContext()).inflate(R.layout.detail_fragment_layout,container,false);
+
+        TextView versionName=view.findViewById(R.id.version_name);
+        return view;
+    }
+}
